e767a8d62ca8d4ec1fba2a496a9f7a632a31cd7c
siosio <siosio12@gmail.com>
2016-04-05 12:08:15 +0900

    。。。

---
+++ ex01-business-rules/src/main/java/kata/ex01/DiscountServiceImpl.java
@@ -0,0 +1,50 @@
+package kata.ex01;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import kata.ex01.model.HighwayDrive;
+
+/**
+ * @author kawasima
+ */
+public class DiscountServiceImpl implements DiscountService {
+
+    private Function<HighwayDrive, Integer> discountHoliday = drive -> {
+        if (drive.isHoliday() && drive.isStandardAndMini() && drive.isRural()) {
+            return 30;
+        }
+        return 0;
+    };
+
+    private Function<HighwayDrive, Integer> discountLateAtNight = drive -> {
+        if (drive.isLateAtNight()) {
+            return 30;
+        }
+        return 0;
+    };
+
+    private Function<HighwayDrive, Integer> discountWeekdayMorningAndEvening = drive -> {
+        if (!drive.isHoliday() && drive.isMorningAndEvening()) {
+            final int countPerMonth = drive.getDriver()
+                                   .getCountPerMonth();
+            if (countPerMonth >= 10) {
+                return 50;
+            } else if (countPerMonth >= 5 && countPerMonth <= 9) {
+                return 30;
+            }
+        }
+        return 0;
+    };
+
+    private List<Function<HighwayDrive, Integer>> discountStrategies =
+            Arrays.asList(discountHoliday, discountLateAtNight, discountWeekdayMorningAndEvening);
+
+    @Override
+    public long calc(HighwayDrive drive) {
+        return discountStrategies.stream()
+                                 .mapToInt(function -> function.apply(drive))
+                                 .max().orElse(0);
+    }
+}
--- ex01-business-rules/src/main/java/kata/ex01/model/HighwayDrive.java
+++ ex01-business-rules/src/main/java/kata/ex01/model/HighwayDrive.java
@@ -1,9 +1,11 @@
 package kata.ex01.model;
 
+import kata.ex01.util.HolidayUtils;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 /**
  * @author kawasima
@@ -16,4 +18,36 @@ public class HighwayDrive implements Serializable {
     private RouteType routeType;
 
     private Driver driver;
+
+    public boolean isHoliday() {
+        return HolidayUtils.isHoliday(enteredAt.toLocalDate()) || HolidayUtils.isHoliday(exitedAt.toLocalDate());
+    }
+
+    public boolean isStandardAndMini() {
+        return vehicleFamily == VehicleFamily.STANDARD
+                || vehicleFamily == VehicleFamily.MINI
+                || vehicleFamily == VehicleFamily.MOTORCYCLE;
+
+    }
+
+    public boolean isRural() {
+        return routeType == RouteType.RURAL;
+    }
+
+    public boolean isLateAtNight() {
+        return isLateAtNight(enteredAt.getHour()) || isLateAtNight(exitedAt.getHour());
+    }
+
+    private boolean isLateAtNight(int hour) {
+        return hour >= 0 && hour <= 4;
+    }
+
+    public boolean isMorningAndEvening() {
+        return isMorningAndEvening(enteredAt.getHour()) || isMorningAndEvening(exitedAt.getHour());
+    }
+
+    private boolean isMorningAndEvening(int hour) {
+        return ((hour >= 6) && (hour <= 9)) || ((hour >= 17) && (hour <= 20));
+    }
+
 }
